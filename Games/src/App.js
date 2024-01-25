fcb0bea08fc68b58ad6e70b409c8987504bf00d2
aditya2894DS <patel.aditya.js2894@gmail.com>
2024-01-25 10:40:54 +0530

    Multiselect files added

---
+++ Games/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MultiSelect from './components/MultiSelect';
+
+function App() {
+  return (
+    <div className="App">
+      <MultiSelect />
+    </div>
+  );
+}
+
+export default App;
